9a044069013f6601c76bfdfad2134766b69e23ed
Pascal SCHIRRMANN <pascal.schirrmann@sacem.fr>
2023-10-30 17:06:59 +0100

    🎉 initial update

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # iTop-Software-EOL
-Add some date Fileds for software and OS Versions
+Add some date Fields for software and OS Versions
+
+# Goal
+
+It is very hard to maintain every bit of codes in a maintained state in a fairly big infrastructure.
+
+The idea is to create some kind of reports with all OS and software, with a state of obsolescence
+
+But by default, Software and OS have no date fields.
+
+So, this contribution aims to add for those products a bunch of new fields :
+
+* Release date
+* Public full support end date
+* Public normal end of support (this is mostly "the" date)
+* Public extended end of support
+* Specific end of support date (in case of a particular situation). If empty, this field 'copy' the 'normal end of support'
+
+
---
+++ schirrms-software-eol/en.dict.schirrms-software-eol.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	// OS Version
+	'Class:OSVersion/Attribute:comment' => 'Comment',
+	'Class:OSVersion/Attribute:date_release' => 'OS Release date',
+	'Class:OSVersion/Attribute:date_end_full_support' => 'OS official end of full support date',
+	'Class:OSVersion/Attribute:date_end_normal_support' => 'OS official end of maintenance support date',
+	'Class:OSVersion/Attribute:date_end_ext_support' => 'OS official end of extended support date',
+	'Class:OSVersion/Attribute:date_end_custom_support' => 'OS company specific end of maintenance support date',
+));
+?>
